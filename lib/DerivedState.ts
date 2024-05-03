ca7cdd427aaba9d32ebc05724b1fcf3ca003a26a
TrueSunGaming <truesungaming@gmail.com>
2024-05-03 22:19:53 +0000

    feat: implement State and DerivedState

---
+++ lib/DerivedState.ts
@@ -0,0 +1,25 @@
+import { GenericFunc } from "./GenericFunc";
+import { MapState, State } from "./State";
+
+export class DerivedState<T, I extends any[]> extends State<T> {
+    private derivation: GenericFunc<I, T>;
+    private states: MapState<I>
+
+    constructor(derivation: GenericFunc<I, T>, states: MapState<I>) {
+        super(derivation(...states.map((v) => v.value) as I));
+
+        this.derivation = derivation;
+        this.states = states;
+
+        for (const i of states) i.changed.connect(() => this.triggerReactivity());
+    }
+
+    private get nextValue(): T {
+        return this.derivation(...this.states.map((v) => v.value) as I);
+    }
+
+    override triggerReactivity(): void {
+        if (this.states) this.setValueNonReactive(this.nextValue);
+        super.triggerReactivity();
+    }
+}
---
+++ lib/State.ts
@@ -0,0 +1,33 @@
+import { Signal } from "./Signal";
+
+export class State<T> {
+    private m_Value: T;
+
+    changed: Signal<[T]> = new Signal();
+
+    constructor(value: T) {
+        this.m_Value = value;
+        this.triggerReactivity();
+    }
+
+    triggerReactivity(): void {
+        this.changed.emit(this.m_Value);
+    }
+
+    get value(): T {
+        return this.m_Value;
+    }
+
+    set value(val: T) {
+        this.m_Value = val;
+        this.triggerReactivity();
+    }
+
+    setValueNonReactive(val: T): void {
+        this.m_Value = val;
+    }
+}
+
+export type MapState<T extends any[]> = {
+    [I in keyof T]: State<T[I]>;
+};
