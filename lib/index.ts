267e791397bd166faeff7209927f8e6f6f64a6a9
TrueSunGaming <79117622+TrueSunGaming@users.noreply.github.com>
2024-06-07 18:44:04 -0400

    feat: reexport missing features

--- lib/index.ts
+++ lib/index.ts
@@ -6,8 +6,12 @@ export * from "./State";
 export * from "./DerivedState";
 export * from "./ReactiveArray";
 export * from "./Factory";
+export * from "./InstanceFactory";
+export * from "./CallFactory"
 export * from "./Matrix";
 export * from "./Result";
 export * from "./dotProduct";
 export * from "./Rotation";
 export * from "./clamp";
+export * from "./Color";
+export * from "./padArray"
